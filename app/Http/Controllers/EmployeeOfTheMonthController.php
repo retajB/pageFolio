f9593774d993607783d4724447ec8be535b36e55
Ahad Al-Ghamdi <ahad.alghamdi89@gmail.com>
2025-05-07 12:35:42 +0300

    new tables

---
+++ app/Http/Controllers/EmployeeOfTheMonthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee_of_the_month;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmployeeOfTheMonthController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employee_of_the_month $employee_of_the_month)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Employee_of_the_month $employee_of_the_month)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Employee_of_the_month $employee_of_the_month)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employee_of_the_month $employee_of_the_month)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SocialMediaSiteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Social_media_site;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SocialMediaSiteController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Social_media_site $social_media_site)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Social_media_site $social_media_site)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Social_media_site $social_media_site)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Social_media_site $social_media_site)
+    {
+        //
+    }
+}
