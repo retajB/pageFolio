fb074b8f969a2b16003f7982706fbc8ba20718c0
Ahad Al-Ghamdi <ahad.alghamdi89@gmail.com>
2025-05-12 09:21:46 +0300

    ادخال معلومات اليوزر

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <title>Admin Control Panel</title>
+  
+</head>
+<body>
+  
+  <h2>User Registration</h2>
+  <form action="#" method="post">
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" name="name" required><br><br>
+
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <label for="phone">Phone Number:</label><br>
+    <input type="tel" id="phone" name="phone" required><br><br>
+
+    <label for="national_id">National ID:</label><br>
+    <input type="text" id="national_id" name="national_id" pattern="\d{10}" required>
+
+
+</body>
+</html>
