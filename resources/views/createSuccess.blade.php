c4916eefc056aa4a5b0574cdc1704fa99a84bb0e
Ahad Al-Ghamdi <ahad.alghamdi89@gmail.com>
2025-05-18 14:37:49 +0300

    .....

--- resources/views/createSuccess.blade.php
+++ resources/views/createSuccess.blade.php
@@ -6,11 +6,11 @@
 </head>
 <body style="text-align: center; direction: ltr; font-family: sans-serif; padding-top: 50px;">
     <h2>Registration Successful 🎉</h2>
-    <p>You will be redirected to the home page shortly...</p>
+    <p>You will be redirected to the edit page shortly...</p>
 
     <script>
     setTimeout(function() {
-        window.location.href = "{{ route('home') }}";
+        window.location.href = "{{ route('edit.page') }}";
     }, 3000);
 </script>
 
