746a7489c60dc480ae845042104cd853071b0cbd
retajB <retajbaaqeel@gmail.com>
2025-07-09 14:12:12 +0300

    تصليحات

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -219,7 +219,6 @@
     Continue <i class="fas fa-arrow-right ms-2"></i>
   </button>
 </div>
-    </form>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
